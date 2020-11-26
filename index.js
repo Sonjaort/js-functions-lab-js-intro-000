0fab2e40b4bc37b10f6ddb6cd18553a2e471de97
Sonja Ortmann <so.ortmann@gmail.com>
2020-11-26 13:33:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // write your code below!
 function happyHolidays() {return "Happy holidays!"}
-function happyHolidaysto () {return `Happy holidays, ${name}!`}
+function happyHolidaysTo () {return `Happy holidays, ${name}!`}
